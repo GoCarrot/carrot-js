2dba3a6543e997114a0f9b4dfc1b91aef8342991
Pat Wilson <zerostride@gmail.com>
2014-04-21 13:45:48 -0700

    Use this to test things.

--- web.js
+++ web.js
@@ -10,6 +10,10 @@ app.get('/', function(request, response) {
   response.sendfile("./lib/Carrot.js");
 });
 
+app.get('/test.html', function(request, response) {
+  response.sendfile("./test.html");
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
